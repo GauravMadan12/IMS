f4a01adc83e3907737dfd044d7699b0490beb8ab
GauravMadan12 <gmadan046@gmail.com>
2018-07-29 12:49:41 +0530

    new files

--- ims/src/app/app.module.ts
+++ ims/src/app/app.module.ts
@@ -2,13 +2,44 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { LoginComponent } from './login/login.component';
+import { RouterModule } from '@angular/router';
+import { NotfoundComponent } from './notfound/notfound.component';
+import { RegisterComponent } from './register/register.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    DashboardComponent,
+    LoginComponent,
+    NotfoundComponent,
+    RegisterComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot([
+      {
+         path:'',
+         component: DashboardComponent
+      },
+      {
+          path:'dashboard',
+          component:DashboardComponent
+      },
+      {
+          path:'login',
+          component:LoginComponent
+      },
+      {
+          path:'register',
+          component:RegisterComponent
+      },
+      {
+          path:'**',
+          component:NotfoundComponent
+      }
+  ])
   ],
   providers: [],
   bootstrap: [AppComponent]
